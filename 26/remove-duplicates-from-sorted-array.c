09c9f114d7867c291b0175afa2b30cfe555f411e
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-03-17 08:56:00 +0800

    26. Remove Duplicates from Sorted Array.c

---
+++ 26/remove-duplicates-from-sorted-array.c
@@ -0,0 +1,11 @@
+int removeDuplicates(int* nums, int numsSize){
+    int k=1;
+    for(int i=1; i<numsSize; i++){
+        if( nums[i] == nums[i-1] ) continue;
+        else{
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+}
