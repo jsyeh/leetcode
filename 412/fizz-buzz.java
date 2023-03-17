6cae160491f90795439441dbf67391a053403088
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-03-18 01:05:00 +0800

    412. Fizz Buzz.java

---
+++ 412/fizz-buzz.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<String>(n);
+        for(int i=1; i<=n; i++){
+            if(i%3==0 && i%5==0) ans.add("FizzBuzz");
+            else if(i%3==0) ans.add("Fizz");
+            else if(i%5==0) ans.add("Buzz");
+            else ans.add(""+i);
+        }
+        return ans;
+    }
+}
