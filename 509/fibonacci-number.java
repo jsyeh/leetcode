e4742144a0bab2c75a270b931e0d82e410f63294
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-03-26 09:17:00 +0800

    509. Fibonacci Number.java

---
+++ 509/fibonacci-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int fib(int n) {
+        int [] fib = new int[40];
+        fib[0] = 0;
+        fib[1] = 1;
+        for(int i=2; i<=n; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        return fib[n];
+    }
+}
