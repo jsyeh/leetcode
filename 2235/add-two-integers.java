ebf38dd543e6ab31b0fcf7fa77c156d4b081d438
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-03-28 23:35:00 +0800

    2235. Add Two Integers.java

---
+++ 2235/add-two-integers.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int sum(int num1, int num2) {
+    //我是被 the-leetcode-beginners-guide 的教學影片帶來這題的
+    //原來 System.out.println() 能在 Console 看到debug結果，太好了
+        //System.out.println("Hello World");
+        return num1 + num2;
+    }
+}
