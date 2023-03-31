957b3c096a9067fc52ace1b69a5906678899e25e
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-03-31 09:06:00 +0800

    217. Contains Duplicate.java

---
+++ 217/contains-duplicate.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i])) return true;
+            else map.put(nums[i], 1);
+        }
+        return false;
+    }
+}
