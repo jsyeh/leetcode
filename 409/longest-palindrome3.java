2fdda84767bdb80e266177c376b74dd1a79c10b7
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-04-11 17:37:00 +0800

    409. Longest Palindrom.java

---
+++ 409/longest-palindrome3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int [] H1 = new int[26];
+        int [] H2 = new int[26];
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c>='A' && c<='Z') H1[c-'A']++;
+            if(c>='a' && c<='z') H2[c-'a']++;
+        }
+        int ans = 0;
+        boolean odd=false;
+        for(int i=0; i<26; i++){
+            if(H1[i]%2==1) odd=true;
+            if(H2[i]%2==1) odd=true;
+            ans += H1[i]/2*2 + H2[i]/2*2;
+        }
+        if(odd) ans++;
+        return ans;
+    }
+}
