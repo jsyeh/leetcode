19fd629ae2467ce752b24c1deb22a850c9b1e072
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-04-15 23:08:00 +0800

    706. Design HashMap.java

---
+++ 706/design-hashmap.java
@@ -0,0 +1,28 @@
+class MyHashMap {
+
+    HashMap<Integer,Integer> map;
+    public MyHashMap() {
+        map = new HashMap<Integer,Integer>();
+    }
+    
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
+    
+    public int get(int key) {
+        if(map.containsKey(key)) return map.get(key);
+        else return -1;
+    }
+    
+    public void remove(int key) {
+        map.remove(key);
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
