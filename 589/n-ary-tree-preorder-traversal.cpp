095ce0fb74d85d4663d9514ee43a6efac5f73b9d
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-05-09 22:21:00 +0800

    589. N-ary Tree Preorder Traversal.cpp

---
+++ 589/n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,32 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> preorder(Node* root) {
+        if(root==nullptr) return ans;
+        ans.push_back(root->val);
+        for(Node * child : root->children){
+            preorder(child);
+        }
+        return ans;
+    }
+};
