b6f46720bee98085e053276a5287c10491c6fb2b
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-06-07 15:22:00 +0800

    1254. Number of Closed Islands.cpp

---
+++ 1254/number-of-closed-islands.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int closedIsland(vector<vector<int>>& grid) {
+        for(int i=0; i<grid.size(); i++){
+            helper(grid, i, 0);
+            helper(grid, i, grid[i].size()-1);
+        }
+        for(int j=0; j<grid[0].size(); j++){
+            helper(grid, 0, j);
+            helper(grid, grid.size()-1, j);
+        }
+
+        int ans = 0;
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[i].size(); j++){
+                if(grid[i][j]==0){
+                    ans++;
+                    helper(grid, i, j);
+                }
+            }
+        }
+        return ans;
+    }
+    void helper(vector<vector<int>>&grid, int i, int j) {
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size()) return;
+
+        if(grid[i][j]==1) return;
+        
+        grid[i][j] = 1;
+        helper(grid, i+1, j);
+        helper(grid, i-1, j);
+        helper(grid, i, j+1);
+        helper(grid, i, j-1);
+    }
+};
