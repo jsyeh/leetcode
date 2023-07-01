5b350c6be7a1ce6ff9ac1cc2b4ebc78484547353
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-07-01 23:09:00 +0800

    365. Water and Jug Problem.c

---
+++ 365/water-and-jug-problem.c
@@ -0,0 +1,12 @@
+int gcd(int a, int b){
+    if(a==0) return b;
+    if(b==0) return a;
+    return gcd(b, a%b);
+}
+bool canMeasureWater(int jug1, int jug2, int total){
+    if(jug1+jug2<total) return false;
+
+    if(jug1==total || jug2==total || jug1+jug2==total) return true;
+
+    return total%gcd(jug1, jug2)==0;
+}
