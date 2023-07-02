63d9be4995423536a29f0b58b9c2954a9c7c1b28
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-07-02 11:07:00 +0800

    70. Climbing Stairs.cpp

---
+++ 70/climbing-stairs.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int a[n+2];
+        a[0] = 1;
+        a[1] = 1;
+        for(int i=2; i<=n; i++) a[i] = a[i-1] + a[i-2];
+        return a[n];
+    }
+};
