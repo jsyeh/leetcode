b71666ad0f51cad4ba8206d2a8cf6eb3e9251aae
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-07-19 06:48:43 +0800

    62. Unique Paths.c

---
+++ 62/unique-paths2.c
@@ -0,0 +1,13 @@
+int uniquePaths(int m, int n){
+    int table[m][n];
+    table[0][0] = 1;
+    for(int i=1; i<m; i++) table[i][0] = table[i-1][0];
+    for(int j=1; j<n; j++) table[0][j] = table[0][j-1];
+
+    for(int i=1; i<m; i++){
+        for(int j=1; j<n; j++){
+            table[i][j] = table[i-1][j] + table[i][j-1];
+        }
+    }
+    return table[m-1][n-1];
+}
