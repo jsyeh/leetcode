6a9e19ffce57efd4e1d627a740d9de124f84bc22
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-07-24 20:39:26 +0800

    50. Pow(x, n).java

--- 50/powx-n.java
+++ 50/powx-n.java
@@ -2,13 +2,10 @@ class Solution {
     public double myPow(double x, int n) {
         if(n==0) return 1;
         if(n==-2147483648){
-            if(x==1) return 1;
-            if(x==-1) return 1;
-            return 0;
-            /*x = 1/x;
-            n -= 1;
-            n = -n;
-            return x*x*myPow(x*x, n/2 );*/
+            return myPow(1/x/x, -(n/2));
+            //因為 test data 多了 case 305/306: 1.0000000000001 -2147483648
+            //這個case太刁鑽了，答案還沒變成0, 而是變成 0.99979
+            //所以程式需要做修改
         }
         if(n<0){
             x = 1/x;
@@ -20,3 +17,4 @@ class Solution {
     }
 }//Case4: 2.00000 -2147483648
 //Case5: -1.00000 -2147483648
+//case 305/306: 1.0000000000001 -2147483648
