41b12a386d20688353367e4cd6fc62ec72bb8081
jsyeh <jsyeh.org@gmail.com>
2023-09-25 20:37:25 +0800

    1822. Sign of the Product of an Array.py

---
+++ 1822/sign-of-the-product-of-an-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        ans = 1
+        for num in nums:
+            if num==0:
+                ans = 0
+            if num>0:
+                ans *= 1
+            if num<0:
+                ans *= -1
+        return ans
