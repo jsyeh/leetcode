5ac2cb38095e28e8c3fa2c78d6fcc13abe787677
jsyeh <jsyeh.org@gmail.com>
2023-10-05 20:15:10 +0800

    1523. Count Odd Numbers in an Interval Range.py

---
+++ 1523/count-odd-numbers-in-an-interval-rang.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        # 這題不能用for迴圈慢慢算，因為數字會很大
+        # 所以就直接數字相減就好了
+        # 奇-奇：1 3 5 7 9 = (9-1)//2 + 1
+        # 偶-奇：0 1 3 5 7 = (7-0)//2 + 1
+        # 奇-偶：1 3 5 6 = (6-1)//2 + 1
+        # 偶-偶：0 1 3 5 6 = (6-0)//2
+        if low%2==0 and high%2==0:
+            return (high-low)//2
+        else:
+            return (high-low)//2 + 1
