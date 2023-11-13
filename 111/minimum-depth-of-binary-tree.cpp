e61d4f5cb445bbe18e677dd70c372bbad3cb964f
jsyeh <jsyeh.org@gmail.com>
2023-11-13 23:43:01 +0800

    111. Minimum Depth of Binary Tree.cpp

---
+++ 111/minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root==nullptr) return 0;
+        int left = minDepth(root->left);
+        int right = minDepth(root->right);
+        if(left==0) return right+1;
+        if(right==0) return left+1;
+        return min(left, right) + 1;
+    }
+};
