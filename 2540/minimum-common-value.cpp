4440f2ab05acd8193e33ba6381e8d9f2ed13fcba
jsyeh <jsyeh.org@gmail.com>
2024-03-10 21:55:35 +0800

    2540. Minimum Common Value.py

---
+++ 2540/minimum-common-value.cpp
@@ -0,0 +1,14 @@
+// 問：nums1 和 nums2 裡面「相同」的數，最小是誰
+// 因 nums1 和 nums2 都 sort() 過了，直接用 i, j 的 two pointers 即可解
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0, j = 0; // 兩個 pointers 針對 nums1[i] 和 nums2[j]
+        while(i<nums1.size() && j<nums2.size()){ // 若還沒超過範圍
+            if(nums1[i]==nums2[j]) return nums1[i]; // 若相等，就找到答案
+            if(nums1[i]<nums2[j]) i++; // 若右邊大，那左邊i++
+            else j++; // 若左邊大，那右邊j +1
+        }
+        return -1; // 沒找到答案
+    }
+};
