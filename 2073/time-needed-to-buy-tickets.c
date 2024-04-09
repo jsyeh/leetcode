f29eaa2dfe40916c6a3da43aca83169edba011d6
jsyeh <jsyeh.org@gmail.com>
2024-04-09 10:11:56 +0800

    2073. Time Needed to Buy Tickets.c

--- 2073/time-needed-to-buy-tickets.c
+++ 2073/time-needed-to-buy-tickets.c
@@ -1,12 +1,17 @@
+// LeetCode 2073. Time Needed to Buy Tickets
+// 今天的題目很簡單, 每個人要買的票數量不同, 每次只能買一張票, 依序買
+// 問第k個人要多久時間, 才能買完他想要的 tickets[k] 個票。
+// 技巧: 「排在他前面的人」最多要一樣多的票, 最少就是「要多少買多少」
+// 「排在他後面的人」最多要買 tickets[k]-1張票, 最少就是「要多少買多少」
 int timeRequiredToBuy(int* tickets, int ticketsSize, int k) {
     int ans = 0;
-    for(int i=0; i<=k; i++){
-        if(tickets[i]<=tickets[k]) ans += tickets[i];
-        else ans += tickets[k];
+    for(int i=0; i<=k; i++){ //排在他前面的人, 包含本人
+        if(tickets[i]<=tickets[k]) ans += tickets[i];//「要多少買多少」
+        else ans += tickets[k]; //一樣多張票
     }
-    for(int i=k+1; i<ticketsSize; i++){
-        if(tickets[i]<tickets[k]) ans += tickets[i];
-        else ans += tickets[k]-1;
+    for(int i=k+1; i<ticketsSize; i++){ //排在他後面的人, 
+        if(tickets[i]<tickets[k]) ans += tickets[i];//「要多少買多少」
+        else ans += tickets[k]-1; //少1張
     }
     return ans;
 }
