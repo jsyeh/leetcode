3b040f0e6c48dc8a5b17e17ff0404033a64422ba
jsyeh <jsyeh.org@gmail.com>
2024-09-10 08:20:31 +0800

    2807. Insert Greatest Common Divisors in Linked List.py

--- 2807/insert-greatest-common-divisors-in-linked-list.py
+++ 2807/insert-greatest-common-divisors-in-linked-list.py
@@ -1,30 +1,15 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    int gcd(int a, int b){
-        if(a==0) return b;
-        if(b==0) return a;
-        return gcd(b, a%b);
-    }
-    public ListNode insertGreatestCommonDivisors(ListNode head) {
-        ListNode ans = head;
-        ListNode next = head.next;
-        while(next!=null){
-            int a = head.val, b = next.val;
-            ListNode mid = new ListNode(gcd(a, b), next);
-            head.next = mid;
-            head = next;
-            next = next.next;
-        }
-        return ans;
-        
-    }
-}
+# LeetCode 2807. Insert Greatest Common Divisors in Linked List
+# 在 Linked List 裡，兩兩相鄰的 node 間，插入「兩人」的最大公因數
+class Solution:
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, now = head, head.next  # 左、右 兩項
+        while now != None:  # 還有右邊項，就可計算gcd()最大公因數
+            prev.next = ListNode(gcd(prev.val,now.val), now)
+            prev, now = now, now.next  # 接好指標後，右移下一組
+        return head
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
