2fe69af3fdfd7071c87346fdf2668f8450556c0a
jsyeh <jsyeh.org@gmail.com>
2024-12-19 22:43:38 +0800

    769. Max Chunks To Make Sorted.cpp

---
+++ 769/max-chunks-to-make-sorted.cpp
@@ -0,0 +1,14 @@
+// LeetCode 769. Max Chunks To Make Sorted
+// 把 arr 分段斷開，分別sort() 再合起來，要是 sorted，問能分幾段
+// 因為數字是 0..n-1 去排列，Hint 1 說前i項的最大值是i就合理
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int ans = 0, nowMax = 0;
+        for(int i=0; i<arr.size(); i++) {
+            nowMax = max(nowMax, arr[i]);
+            if(nowMax==i) ans++;
+        }
+        return ans;
+    }
+};
