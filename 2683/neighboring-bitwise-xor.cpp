8daa319f37df9733a4f4e5061f7b43cd07b3d704
jsyeh <jsyeh.org@gmail.com>
2025-01-17 09:23:14 +0800

    2683. Neighboring Bitwise XOR.cpp

---
+++ 2683/neighboring-bitwise-xor.cpp
@@ -0,0 +1,14 @@
+// LeetCode 2683. Neighboring Bitwise XOR
+// derived[i] = original[i] XOR original[i+1]）
+// 給你 derived 問「有沒有可能」有這樣的 original 陣列。
+// 因XOR 做兩次=沒做，所以 derived 全部 XOR 起來 = original 全部 XOR 做兩次 = 0
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        int total = 0; // 迴圈前面 total 是 0 
+        for(int now : derived) {
+            total ^= now; // 全部 XOR 起來，放入 total
+        }
+        return total==0; // 看最後 total 是不是 0 
+    }
+};
