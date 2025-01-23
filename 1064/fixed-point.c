dc841ca61d678bb0084c39395cc1bad8e10d999a
jsyeh <jsyeh.org@gmail.com>
2025-01-23 10:56:44 +0800

    1064. Fixed Point.c

---
+++ 1064/fixed-point.c
@@ -0,0 +1,10 @@
+// LeetCode 1064. Fixed Point
+// arr 已經小到大排序好，想找到 arr[i]==i 的座標
+int fixedPoint(int* arr, int arrSize) {
+    // 最簡單的方法，直接用迴圈測
+    for(int i=0; i<arrSize; i++) {
+        if(arr[i]==i) return i; // 符合題目條件
+        if(arr[i]>i) break; // 數字跳超過、沒機會了
+    }
+    return -1; // 失敗
+}
