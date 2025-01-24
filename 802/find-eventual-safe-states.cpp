3035e6897b9f70c256915280d6ce77edc66d5873
jsyeh <jsyeh.org@gmail.com>
2025-01-24 23:12:42 +0800

    802. Find Eventual Safe States.cpp

--- 802/find-eventual-safe-states.cpp
+++ 802/find-eventual-safe-states.cpp
@@ -1,22 +1,19 @@
+// LeetCode 802. Find Eventual Safe States
+// graph[i] 記錄 node i 可到達的其他 nodes。terminal node 是終點（出不去）
+// safe node 「安全的」點，是「出發後，能走到終點」。將「安全的」點全部找出來
 class Solution {
 public:
-    vector<int> ans;
-    vector<int> terminal;
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        vector<int> ans;
         int N = graph.size();
         vector<int> state(N, 0); //0: unvisited, 1: unsafe, 2: safe
-
         //這個演算法很特別：經過但未確認的點，都先當 unsafe, 
         //所以再度踩到自己時，便「確認」有loop而「不安全」，可以 return
         for(int i=0; i<graph.size(); i++){
             if( dfsCheckSafe(graph, state, i) == true ) ans.push_back(i);
         }
-
-        //sort(ans.begin(), ans.end()); //題目說結果需要排序好
-        //因為程式的迴圈是從小到大去檢查，所以已經排序好了，不用再排序一次
         return ans;
     }
-
     bool dfsCheckSafe(vector<vector<int>>& graph, vector<int>& state, int i) {
         //state[i] 0: unvisited, 1: unsafe, 2: safe
         if(state[i]==1) return false; //有走到 unsafe 包含可能有loop發生
@@ -29,9 +26,6 @@ public:
         }
         //都檢測通過（沒有走到險地）就是安全
         state[i] = 2; //safe
-
         return true;//safe
-    }
-
-    
+    }  
 };
