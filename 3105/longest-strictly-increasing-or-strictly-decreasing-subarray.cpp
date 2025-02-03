d1d6058a4c69ae174911c74d8fe1cd784011bbf0
jsyeh <jsyeh.org@gmail.com>
2025-02-03 12:43:52 +0800

    3105. Longest Strictly Increasing or Strictly Decreasing Subarray.cpp

---
+++ 3105/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -0,0 +1,20 @@
+// LeetCode 3105. Longest Strictly Increasing or Strictly Decreasing Subarray
+// 持續增加 or 持續減少，最長的長度是多少？
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int ans = 1, inc = 1, dec = 1;
+        for(int i=1; i<nums.size(); i++) {
+            if(nums[i-1]<nums[i]) { // 持續增加
+                inc++;
+                ans = max(ans, inc);
+            } else inc = 1; // 沒有持續增加，重來
+
+            if(nums[i-1]>nums[i]) { // 持續減少
+                dec++;
+                ans = max(ans, dec);
+            } else dec = 1; // 沒有持續減少，重來
+        }
+        return ans;
+    }
+};
