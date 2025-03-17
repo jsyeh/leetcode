dfb8587df77b1d7222506241e0681300fa2152fc
jsyeh <jsyeh.org@gmail.com>
2025-03-17 08:12:00 +0800

    2206. Divide Array Into Equal Pairs.py

--- 2206/divide-array-into-equal-pairs.py
+++ 2206/divide-array-into-equal-pairs.py
@@ -1,7 +1,9 @@
-# nums[i] 能否「兩兩一組」分光光？
+# LeetCode 2206. Divide Array Into Equal Pairs
+# 將 nums 裡的數，相同的數「兩兩一組」分好，全部分完。
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
-        counter = Counter(nums) # 統計 nums 出現狀況
-        for k in counter: # 對每一個數
-            if counter[k]%2==1: return False # 只要有落單，就失敗
-        return True # 沒有失敗
+        counter = Counter(nums)  # 統計 nums 出現狀況
+        for k in counter:  # 對每一個數
+            if counter[k]%2==1:
+                return False  # 只要有落單，就失敗
+        return True  # 沒有失敗
