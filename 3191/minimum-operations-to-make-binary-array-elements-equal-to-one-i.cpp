8f50853bf634ee3f14506487b24215489f8f3d29
jsyeh <jsyeh.org@gmail.com>
2025-03-19 14:53:47 +0800

    3191. Minimum Operations to Make Binary Array Elements Equal to One I.cpp

---
+++ 3191/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -0,0 +1,18 @@
+// LeetCode 3191. Minimum Operations to Make Binary Array Elements Equal to One I
+// 想把 nums 裡的數，每次「切換3個相鄰的數」0與1互換，要做幾次「3個數翻牌」才能全部變成1
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int ans = 0; // 要「翻牌翻幾次」
+        for(int i=0; i<nums.size()-2; i++) {
+            if(nums[i]==0){ // 遇到0要翻牌
+                ans++; // 翻牌加一次
+                nums[i+1] = 1 - nums[i+1]; //反過來
+                nums[i+2] = 1 - nums[i+2]; //反過來
+            }
+        }
+        int N = nums.size();
+        if(nums[N-1]==0 || nums[N-2]==0) return -1; //最後有0失敗
+        return ans; //可以成功「翻牌」全部變成1
+    }
+};
