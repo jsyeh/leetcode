224e352a90892deb5248382584ffed64e2d4f1ce
jsyeh <jsyeh.org@gmail.com>
2025-04-17 17:53:16 +0800

    2176. Count Equal and Divisible Pairs in an Array.py

--- 2176/count-equal-and-divisible-pairs-in-an-array.py
+++ 2176/count-equal-and-divisible-pairs-in-an-array.py
@@ -1,10 +1,11 @@
-# 找出 nums[i]==nums[j] 且 i*j%k==0
+# LeetCode 2176. Count Equal and Divisible Pairs in an Array
+# 請問有幾組 nums[i]==nums[j] and (i*j)%k==0
 class Solution:
     def countPairs(self, nums: List[int], k: int) -> int:
         N = len(nums)
         ans = 0
-        for i in range(N):
-            for j in range(i+1,N):
-                if nums[i]==nums[j] and i*j%k==0:
+        for i in range(N):  # 左手i
+            for j in range(i+1,N):  # 右手j 直接暴力試即可
+                if nums[i]==nums[j] and (i*j)%k==0:
                     ans += 1
         return ans
