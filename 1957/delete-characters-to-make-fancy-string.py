6ef9090769e3885e8ea905d85f8a5f0483f788c0
jsyeh <jsyeh.org@gmail.com>
2025-07-21 11:22:00 +0800

    1957. Delete Characters to Make Fancy String.py

--- 1957/delete-characters-to-make-fancy-string.py
+++ 1957/delete-characters-to-make-fancy-string.py
@@ -3,14 +3,14 @@
 # 就直接 for迴圈，如果重覆的字 < 3 就 ans.append(c) 放入答案
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        prevC, prevN = 0, 0  # 之前出過的字母
+        prevC, prevN = 0, 0  # 前一次出現的字母、累積出現幾次
         ans = []  # 使用 list 會比字串快。最後再 ''.join(ans)
         for c in s:  # 逐個字母處理
-            if c == prevC:  # 有相同時，累加
+            if c == prevC:  # 字母相同時，累加「累積出現次數」
                 prevN += 1  # 次數+1
-            else:  # 不相同時，換新的字母
+            else:  # 字母不相同時，換新的字母
                 prevC = c  # 換新的字母
-                prevN = 1  # 次數變1個
+                prevN = 1  # 「累積出現次數」變1次
             if prevN < 3:  # 只要不超過3個，就可加入 ans
                 ans.append(c)
-        return ''.join(ans)
+        return ''.join(ans)  # 最後再把 ans 變回「字串」
