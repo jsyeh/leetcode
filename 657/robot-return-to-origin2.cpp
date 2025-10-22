69a75c72451a92e779628fff2803d5fe12bce604
jsyeh <jsyeh.org@gmail.com>
2025-10-22 10:28:00 +0800

    657. Robot Return to Origin.cpp

--- 657/robot-return-to-origin2.cpp
+++ 657/robot-return-to-origin2.cpp
@@ -1,17 +1,24 @@
-// week06-3.cpp 學習計畫 Simulation 第2題
 // LeetCode 657. Robot Return to Origin
+// 模擬機器人走路
+// 'U' up往上, 'D' down往下, 'L' left往左, 'R'right往右
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        int x = 0, y = 0;
-        for(char c : moves){
-            if(c=='U') y++;
-            if(c=='D') y--;
-            if(c=='L') x--;
-            if(c=='R') x++;
-        }
-
-        if(x==0 && y==0) return true;
-        return false;
+        int x = 0, y = 0; // 一開始的位置, 在 (0,0)
+        //for (int i=0; i<moves.length(); i++) { // 傳統的 for 迴圈
+        //    char c = moves[i];             // 取出字串裡第i個字母
+        for (char c : moves) { // C++ 進階迴圈
+            if (c=='U') { // up往上
+                y++;
+            } else if (c=='D') { // down往下
+                y--;
+            } else if (c=='L') { // left往左
+                x--;
+            } else if (c=='R') { // right往右
+                x++;
+            }
+        } // 離開迴圈後
+        if (x==0 && y==0) return true;
+        else return false;
     }
 };
