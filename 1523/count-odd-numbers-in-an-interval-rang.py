4c249b0c5f57ff40d65902cd4c1688cbd601076f
jsyeh <jsyeh.org@gmail.com>
2025-12-07 08:48:00 +0800

    1523. Count Odd Numbers in an Interval Range.py

--- 1523/count-odd-numbers-in-an-interval-rang.py
+++ 1523/count-odd-numbers-in-an-interval-rang.py
@@ -1,11 +1,12 @@
+# LeetCode 1523. Count Odd Numbers in an Interval Range
+# 請問 low ... high 這個範圍裡，有幾個「奇數」？
+# 這題不能用for迴圈慢慢算，因為數字會很大。直接數字相減就好了。
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
-        # 這題不能用for迴圈慢慢算，因為數字會很大
-        # 所以就直接數字相減就好了
         # 奇-奇：1 3 5 7 9 = (9-1)//2 + 1
         # 偶-奇：0 1 3 5 7 = (7-0)//2 + 1
         # 奇-偶：1 3 5 6 = (6-1)//2 + 1
-        # 偶-偶：0 1 3 5 6 = (6-0)//2
+        # 偶-偶：0 1 3 5 6 = (6-0)//2 不用再加一
         if low%2==0 and high%2==0:
             return (high-low)//2
         else:
