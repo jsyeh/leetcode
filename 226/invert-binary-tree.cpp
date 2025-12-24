1f4a1e7e30784a8b782b82f937c15eb5abf0ae14
jsyeh <jsyeh.org@gmail.com>
2025-12-24 09:55:00 +0800

    226. Invert Binary Tree.cpp

--- 226/invert-binary-tree.cpp
+++ 226/invert-binary-tree.cpp
@@ -1,3 +1,15 @@
+// LeetCode 226. Invert Binary Tree 左右對調
+class Solution {
+public: // 已經有個函式, 寫好它, 就可「函式呼叫函式」
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==nullptr) return root; // 終止條件
+        TreeNode * left = invertTree(root->right); // 處理左邊的樹
+        TreeNode * right = invertTree(root->left); // 處理右邊的樹
+        root->left = left; // 將右邊放左邊
+        root->right = right; // 將左邊放右邊
+        return root;
+    }
+};
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,14 +21,3 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    TreeNode* invertTree(TreeNode* root) {
-        if(root==nullptr) return root;
-        TreeNode * left = invertTree(root->right);
-        TreeNode * right = invertTree(root->left);
-        root->left = left;
-        root->right = right;
-        return root;
-    }
-};
