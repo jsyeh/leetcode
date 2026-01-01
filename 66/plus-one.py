1b5b63806f61b789511f3565dd5c33339b01cf1a
jsyeh <jsyeh.org@gmail.com>
2026-01-01 11:33:00 +0800

    66. Plus One.py

--- 66/plus-one.py
+++ 66/plus-one.py
@@ -2,11 +2,9 @@
 # 把 digits 對應的數 +1，不過要小心「進位」的問題，位數可能會多1位
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        N = len(digits)
-        carry = 1
-        for i in range(N-1, -1, -1):
-            digits[i] += carry
-            carry = digits[i] // 10
-            digits[i] %= 10
-        if carry==0: return digits  # 最後沒有進位
-        return [carry] + digits  # 最後還有進位，放在更前面
+        ans, carry = 0, 1  # 把「要加1」的1藏在carry裡
+        for i in range(len(digits)-1, -1, -1):  # 反過來的迴圈
+            carry += digits[i]  # 現在這個位數的加總
+            carry, digits[i] = carry//10, carry%10  # 進位、留在原位
+        if carry>0: return [1] + digits  # 若最後還有進位，就加長1格
+        return digits  # 沒額外進位，直接回傳答案
