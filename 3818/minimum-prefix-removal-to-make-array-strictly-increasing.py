59cddb3efa7dd176a5ccf6af6eb804dbcc8bfb72
jsyeh <jsyeh.org@gmail.com>
2026-01-25 13:30:00 +0800

    3818. Minimum Prefix Removal to Make Array Strictly Increasing.py

---
+++ 3818/minimum-prefix-removal-to-make-array-strictly-increasing.py
@@ -0,0 +1,6 @@
+# LeetCode 3818. Minimum Prefix Removal to Make Array Strictly Increasing
+class Solution:
+    def minimumPrefixLength(self, nums: List[int]) -> int:
+        for i in range(len(nums)-2, -1, -1):
+            if nums[i] >= nums[i+1]: return i+1
+        return 0
